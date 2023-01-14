539f2095dca2a4868a951efaec14d0b480ad1807
Maria José Solano <majosolano99@gmail.com>
2023-01-13 16:11:59 -0800

    Add lint implementation

--- clippy_lints/src/foo_functions.rs
+++ clippy_lints/src/foo_functions.rs
@@ -1,4 +1,5 @@
-use rustc_ast::ast::*;
+use clippy_utils::diagnostics::span_lint_and_help;
+use rustc_ast::visit::FnKind;
 use rustc_lint::{EarlyContext, EarlyLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 
@@ -18,8 +19,33 @@ declare_clippy_lint! {
     #[clippy::version = "1.68.0"]
     pub FOO_FUNCTIONS,
     pedantic,
-    "default lint description"
+    "function named `foo`, which is not a descriptive name"
 }
+
 declare_lint_pass!(FooFunctions => [FOO_FUNCTIONS]);
 
-impl EarlyLintPass for FooFunctions {}
+impl EarlyLintPass for FooFunctions {
+    // fn check_fn(&mut self, cx: &EarlyContext<'_>, fn_kind: FnKind<'_>, span: Span, _: NodeId) {
+    //     // TODO: Emit lint here
+    // }
+    fn check_fn(&mut self, cx: &EarlyContext<'_>, fn_kind: FnKind<'_>, span: rustc_span::Span, _: rustc_ast::NodeId) {
+        if is_foo_fn(fn_kind) {
+            span_lint_and_help(
+                cx,
+                FOO_FUNCTIONS,
+                span,
+                "function named `foo`",
+                None,
+                "consider using a more meaningful name",
+            );
+        }
+    }
+}
+
+fn is_foo_fn(fn_kind: FnKind<'_>) -> bool {
+    match fn_kind {
+        FnKind::Fn(_, ident, ..) => ident.name.as_str() == "foo",
+        // Ignore closures
+        FnKind::Closure(..) => false,
+    }
+}
